d52b5476428eb6b4b3545051d00da6be081f402c
iyidaclem <iyidaclem@gmail.com>
2022-01-07 11:17:08 +0100

    Changes made

--- contract-data/dast.sol
+++ contract-data/dast.sol
@@ -443,7 +443,7 @@ abstract contract BaseToken {
 
 // Root file: contracts/standard/StandardToken.sol
 
-pragma solidity =0.8.4;
+pragma solidity >=0.8.4;
 
 // import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
